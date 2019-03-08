15aeebf6c6b22a6c372727fd03496677fc3ad6e9
Umadevi23 <uma_gorakati@yahoo.co.in>
2019-03-08 14:02:09 +0000

    JavaIfAndElse

---
+++ src/love/programming/ElseAndIf.java
@@ -0,0 +1,15 @@
+package love.programming;
+
+public class ElseAndIf {
+    public static void main(String[] args) {
+        int x =20;
+        int y =20;
+        if( x < y)
+            System.out.println("x is less than y");
+        else if (x == y)
+            System.out.println("x is equal to y");
+        else if(x > y)
+            System.out.println("x is greater than y");
+
+    }
+}
